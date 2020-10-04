ae2730b83dbb58093a71147740862028f17c8e7d
Jacob-Connelly <jacob.connelly88@gmail.com>
2020-10-03 19:58:46 -0600

    Logic finished, need to pretty it up

---
+++ Rock/RockPaperScissors.py
@@ -0,0 +1,38 @@
+game = True;
+
+while game:
+    while game:
+        print("Please input Rock, Paper, or Scissors")
+        p1 = input("Player 1:")
+        p2 = input("Player 2:")
+
+        if p1 == p2:
+            print("tie")
+        elif (p1 == "Rock" and p2 == "Paper") or (p2 == "Rock" and p1 == "Paper"):
+            if p1 == "Paper":
+                print("Player 1 wins!")
+                break
+            else:
+                print("Player 2 wins!")
+                break
+        elif (p1 == "Scissors" and p2 == "Paper") or (p1 == "Paper" and p2 == "Scissors"):
+            if p1 == "Scissors":
+                print("Player 1 wins!")
+                break
+            else:
+                print("Player 2 wins!")
+                break
+        else:
+            if p1 == "Rock":
+                print("Player 1 wins!")
+                break
+            else:
+                print("Player 2 wins!")
+                break
+
+    play = input("Would you like to play again(y/n)?")
+    if play == "y":
+        print("New Game Started")
+    else:
+        print("Game Ended")
+        break
